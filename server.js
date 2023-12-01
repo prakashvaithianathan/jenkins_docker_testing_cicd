d8abc859540f561bb333744fd8bcc190e1cc841f
Prakash Vaithianathan <60776039+prakashvaithianathan@users.noreply.github.com>
2023-12-02 03:35:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Welcome to the app!");
+    res.send("Hello World, Welcome to the app!");
 })
 
 app.listen(5000, () => {
